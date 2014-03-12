8d9cb71f6e738afdb891e98abc944aebbf581a27
Mike Naberezny <mike@naberezny.com>
2014-03-12 10:04:28 -0700

    Prepare 0.19 release

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,7 @@ copyright = u'2008-%d, Mike Naberezny and contributors' % year
 # built documents.
 #
 # The short X.Y version.
-version = '0.19-dev'
+version = '0.19'
 # The full version, including alpha/beta/rc tags.
 release = version
 
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-__version__ = '0.19-dev'
+__version__ = '0.19'
 
 import os
 import sys
