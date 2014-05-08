6c45bc530570835c58a5f5cc656a6b976fe0fddb
Mike Naberezny <mike@naberezny.com>
2014-05-08 14:41:12 -0700

    Prepare 0.20 release

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,7 @@ copyright = u'2008-%d, Mike Naberezny and contributors' % year
 # built documents.
 #
 # The short X.Y version.
-version = '0.20-dev'
+version = '0.20'
 # The full version, including alpha/beta/rc tags.
 release = version
 
